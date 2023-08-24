2ecc7356819b581d850543260ba840c503f5d7e0
Erick Coutinho <erickcoutinho08@hotmail.com>
2023-08-23 21:48:04 -0300

    obtendo dados com php

--- execphp/cad.php
+++ execphp/cad.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="phpcss.css">
     <title>Resultado</title>
 </head>
 <body>
@@ -13,8 +13,11 @@
     </header>
     <main>
         <?php 
-        
+        $n = $_GET["nome"];
+        $s = $_GET["sobrenome"];
+        echo "<p>É um prazer te conhcer, <strong> $n $s!</strong> este é o meu site</p>"
         ?>
+    <p><a href="javascript:history.go(-1)">Voltar para página anterior</a></p>    
     </main>
     
 </body>
