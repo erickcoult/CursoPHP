0eecea77a269e724ab4361bfda00102bae0ac589
Erick Coutinho <erickcoutinho08@hotmail.com>
2023-10-09 21:52:29 -0300

    finalização

--- execphp/exec2/cadexe2.php
+++ execphp/exec2/cadexe2.php
@@ -9,13 +9,19 @@
 <body>
     <main>
        <?php  
-        $cotacao = 5.17;
+        $início = date("m-d-Y", strtotime("-7 days"));
+        $fim = date("m-d-Y");
+        $url = 'https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata/CotacaoDolarPeriodo(dataInicial=@dataInicial,dataFinalCotacao=@dataFinalCotacao)?@dataInicial=\''. $início .'\'&@dataFinalCotacao=\''. $fim .'\'&$top=1&$orderby=dataHoraCotacao%20desc&$format=json&$select=cotacaoCompra,dataHoraCotacao';
+
+        $dados = json_decode(file_get_contents($url), true);
+        $cotação = $dados["value"]["0"]["cotacaoCompra"];
+
         $real = $_REQUEST["din"] ?? 0;
-        $dolar = $real / $cotacao;
+        $dolar = $real / $cotação;
         $fmt = numfmt_create('pt_BR', NumberFormatter::CURRENCY);
 
         echo "Seus " . numfmt_format_currency($fmt, $real, "BRL") . " equivalem a <strong>" . numfmt_format_currency($fmt, $dolar, "USD") . "</strong><br>";
-        echo "Cotação do dolar: <strong>R$5,17</strong>"
+        echo "Cotação do dólar:$" . $dados["value"][0]["cotacaoCompra"];
         ?>
         <button onclick="javascript:history.go(-1)">Voltar</button>
     </main>
