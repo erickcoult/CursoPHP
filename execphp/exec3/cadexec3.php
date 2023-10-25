bbe3990eeac5effc45ac0c3bb756ff2cc5649f25
Erick Coutinho <erickcoutinho08@hotmail.com>
2023-10-25 20:40:16 -0300

    A anatomia da divisão

--- execphp/exec3/cadexec3.php
+++ execphp/exec3/cadexec3.php
@@ -1,13 +1,11 @@
 <!DOCTYPE html>
 <html lang="pt-br">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Forumário retroalimentado</title>
     <link rel="stylesheet" href="phpcss.css">
 </head>
-
 <body>
     <?php
     $valor1 = $_GET['v1'] ?? 0;
