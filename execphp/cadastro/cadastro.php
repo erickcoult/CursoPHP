6a4347ee7faba42242ac40af62fbd4a38c43332d
Erick Coutinho <erickcoutinho08@hotmail.com>
2024-04-03 21:32:19 -0300

    update

--- execphp/cadastro/cadastro.php
+++ execphp/cadastro/cadastro.php
@@ -1,5 +1,16 @@
 <?php 
-include('conexao.php')
+include('conexao.php');
+ if(isset($_POST ['email']) || isset($_POST['senha'])) {
+
+    if(strlen($_POST['email'] == 0)){
+        echo "Preencha o e-mail";
+    } else if (strlen($_POST['senha']) == 0){
+        echo "Preencha sua senha";
+    } else {
+        $email = $mysqli -> real_escape_string($_POST['email']);
+        $senha = $mysqli -> real_escape_string($_POST['senha']);
+    }
+ }
 ?>
 
 <!DOCTYPE html>
