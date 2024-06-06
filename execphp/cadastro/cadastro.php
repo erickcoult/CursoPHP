c6f732a1a64c46ebab3323cbdbf9a67d808b84ed
Erick Coutinho <erickcoutinho08@hotmail.com>
2024-06-06 03:32:50 -0300

    Update cadastro.php

--- execphp/cadastro/cadastro.php
+++ execphp/cadastro/cadastro.php
@@ -1,66 +1,48 @@
 <?php 
 include('conexao.php');
- if(isset($_POST ['email']) || isset($_POST['senha'])) {
 
-    if(strlen($_POST['email'] == 0)){
-        echo "Preencha o e-mail";
-    } else if (strlen($_POST['senha']) == 0){
-        echo "Preencha sua senha";
+if (isset($_POST['email']) || isset($_POST['senha'])) {
+    if(strlen($_POST['email']) == 0){
+        echo "Preencha seu E-mail";
+
+    } else if(strlen($_POST['senha']) == 0){
+        echo "Preebcha sua Senha";
     } else {
-        $email = $mysqli -> real_escape_string($_POST['email']);
-        $senha = $mysqli -> real_escape_string($_POST['senha']);
-    }
- }
-    $sql_code = "SELECT * FROM usuarios WHERE email = '$email' AND senha = '$senha'";
-    $sql_query = $mysqli ->query($sql_code) or die("Falha na execução do código" . $mysqli -> error);
-
-    $quantidade = $sql_query ->num_rows;
-
-    if($quantidade ==1) {
-
-        $usuario = $sql_query->fetch_assoc();
-
-        if(!isset($_SESSION)) {
-            session_start();
-        }
-
-        $_SESSION['id'] = $usuario['id'];
-        $_SESSION['nome'] = $usuario['nome'];
         
-        header("location: painel.php");
+        $email = $mysqli->real_escape_string($_POST['email']);
+        $senha =$mysqli->real_escape_string($_POST['senha']);
 
-    } else{
-        echo "Falha ao logar! E-mail ou senhas incorretos";
+        $sql_code = "SELECT * FROM usuarios WHERE email = '$email' AND senha ='$senha'";
     }
+}
 ?>
-
+asdsa
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Cadastro</title>
-</head>
+</head> 
 <body>
 <h1>Cadastro</h1>
-    <form action="" method="post">
+    <form action="" method="POST">
         <p>
             <label for="email">E-mail</label>
-            <input type="text" name="email" id="">
+            <input type="text" name="email">
         </p>
         <p>
-            <label for="password">Senha</label>
-            <input type="password" name="password" id="">
+            <label for="senha">Senha</label>
+            <input type="password" name="senha">
         </p>
         <button type="submit">Entrar</button>
     </form>
 
-    <h1>Passo a passo pra criar cadastro</h1>
+    <h1>Passo a passo para criar cadastro</h1>
     <ol>
-        <li>Criar conexão com a tabela Cadastro </li>
-        <li>Criar validação se existe os campos email e senha</li>
+        <li>Criar conexão com a tabela Cadastro</li>
+        <li>Criar validação, se existem os campos email e senha</li>
         <li>Criar validação se os campos não estão vazios</li>
-        <li></li>
     </ol>
 </body>
 </html>
